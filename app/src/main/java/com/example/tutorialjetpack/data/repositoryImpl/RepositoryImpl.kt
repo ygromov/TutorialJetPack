5621d91efcf451182397f4a198d63f96b967cac9
test <gdcool@inbox.ru>
2023-06-05 18:27:32 +0300

    Add DataStore Add getUserOfp

---
+++ app/src/main/java/com/example/tutorialjetpack/data/repositoryImpl/RepositoryImpl.kt
@@ -0,0 +1,82 @@
+package com.example.tutorialjetpack.data.repositoryImpl
+
+import android.util.Log
+import com.example.tutorialjetpack.data.datastore.AppDataStore
+import com.example.tutorialjetpack.data.local.OfpDao
+import com.example.tutorialjetpack.data.local.entity.toUserEntity
+import com.example.tutorialjetpack.data.local.entity.toUserModel
+import com.example.tutorialjetpack.domain.model.OfpModel
+import com.example.tutorialjetpack.domain.model.UserModel
+import com.example.tutorialjetpack.domain.repository.Repository
+import com.example.tutorialjetpack.local.toOfpEntity
+import com.example.tutorialjetpack.local.toOfpModel
+import com.example.tutorialjetpack.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+private const val TAG = "RepositoryImpl"
+
+class RepositoryImpl @Inject constructor(
+    //sharefPref
+    private val appDataStoreManager: AppDataStore,
+    private val ofpDao: OfpDao
+) : Repository {
+
+    override fun addUserData(user: UserModel): Flow<Resource<UserModel>> = flow {
+        emit(Resource.Loading())
+        try {
+            val userEntity = user.toUserEntity()
+            val id = ofpDao.insertUserEntity(userEntity)
+            //sharedPref save id
+            appDataStoreManager.setValue("userId", id)
+            emit(Resource.Success(userEntity.toUserModel()))
+        } catch (ex: Exception) {
+            Log.d(TAG, "addUserData: $ex")
+            emit(Resource.Error(message = "Error"))
+        }
+        emit(Resource.Loading(isLoading = false))
+    }
+
+    override fun addOfpData(ofp: OfpModel): Flow<Resource<OfpModel>> = flow {
+        emit(Resource.Loading())
+        try {
+            val ofpEntity = ofp.toOfpEntity() //конвертировали в ентити
+            ofpDao.insertOfpEntity(ofpEntity)           //отправили данные в БД
+            emit(Resource.Success(ofpEntity.toOfpModel())) // отправили данные во флоу
+        } catch (ex: Exception) {
+            emit(Resource.Error(message = "Error"))
+        }
+        emit(Resource.Loading(isLoading = false))
+    }
+
+    override suspend fun getId(): Long? {
+        return appDataStoreManager.readValue("userId")
+    }
+
+    // здесь переопределенный метод из repository, в котором по id нужно получить список с данными
+    override suspend fun getUserOfp(): Flow<Resource<OfpModel>> = flow {
+        emit(Resource.Loading())
+
+        try {
+            val userId = appDataStoreManager.readValue("userId")
+            val ofpEntity = ofpDao.getUserOfp(userId = userId!!.toInt()) //взяли данные из БД, это офпЭнтити
+
+            Log.d(TAG, "getUserOfp: $ofpEntity")
+            emit(Resource.Success(ofpEntity.toOfpModel()))
+//            val userWithOfp = userWithOfpList.firstOrNull() //проверили на нулл
+
+//            if (userWithOfp != null) {
+//                val ofpModelList = userWithOfp.ofps.map { it.toOfpModel() } // данные конвертировали в ofpModel и положили в ЛИСТ
+//
+//                emit(Resource.Success(ofpModelList)) // отправили ЛИСТ во флоу по id
+//            } else {
+//                emit(Resource.Error(message = "User with id $id not found"))
+//            }
+        } catch (ex: Exception) {
+            emit(Resource.Error(message = "Error"))
+        } finally {
+            emit(Resource.Loading(isLoading = false))
+        }
+    }
+}
