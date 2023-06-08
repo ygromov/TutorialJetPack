16d64a31ef69f50fb3c624a83869c1953038bd69
test <gdcool@inbox.ru>
2023-06-08 11:25:45 +0300

    Change getUserId because List not need

--- app/src/main/java/com/example/tutorialjetpack/presentation/screens/exercise_screen/ExerciseViewModel.kt
+++ app/src/main/java/com/example/tutorialjetpack/presentation/screens/exercise_screen/ExerciseViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.tutorialjetpack.presentation.screens.exercise_screen
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.tutorialjetpack.domain.model.OfpFieldModel
 import com.example.tutorialjetpack.domain.repository.Repository
+import com.example.tutorialjetpack.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -20,10 +23,33 @@ class ExerciseViewModel @Inject constructor(
         getUserOfpData()
     }
 
+
     fun getUserOfpData() {
         viewModelScope.launch {
             repository.getUserOfp().collect {
-                state.list.get(0).value = it.data?.push ?: -1
+                    //resource ->
+//                when (resource) {
+//                    is Resource.Success -> {
+//                        val ofpModel = resource.data                                        //записали данные из бд в переменную
+//                        val updatedList = state.list.map { fieldModel ->            //замапили данные в List переменную
+//                            when (fieldModel.nameExerscise) {
+//                                "pushUp" -> ofpModel.let { fieldModel.copy(value = it.push) }
+//                                "pullUp" -> ofpModel.let { fieldModel.copy(value = it.pull) }
+//                                "squat" -> ofpModel.let { fieldModel.copy(value = it.squat) }
+//                                "abc" -> ofpModel.let { fieldModel.copy(value = it.abc) }
+//                                "extens" -> ofpModel.let { fieldModel.copy(value = it.extens) }
+//                                else -> fieldModel
+//                            }
+//                        }
+//                        state = mutableStateListOf(*updatedList.toTypedArray()).let { state.copy(list = it) }
+//                    }
+//                    is Resource.Error -> {
+//                        // Обработка ошибки, если необходимо
+//                    }
+//                    is Resource.Loading -> {
+                        // Обработка состояния загрузки, если необходимо
+//                    }
+//                }
             }
         }
     }
