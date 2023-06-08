a748557e28a7500c843c0a7ca57497919a112633
test <gdcool@inbox.ru>
2023-06-08 11:24:12 +0300

    Change getUserId because List not need

--- app/src/main/java/com/example/tutorialjetpack/presentation/screens/exercise_screen/components/ExerciseField.kt
+++ app/src/main/java/com/example/tutorialjetpack/presentation/screens/exercise_screen/components/ExerciseField.kt
@@ -1,12 +1,17 @@
 package com.example.tutorialjetpack.presentation.screens.exercise_screen.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import com.example.tutorialjetpack.domain.model.ExerciseFieldModel
 
 @Composable
@@ -15,13 +20,21 @@ fun ExerciseField(
     //state2: MutableStateFlow<Resource<OfpModel>>
 ) {
     Card() {
-        Row() {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, top = 10.dp, end = 10.dp)
+        ) {
             Icon(
                 imageVector = ImageVector.vectorResource(id = exerciseFieldModel.icon),
                 contentDescription = "image_icon_exercise"
             )
             Text(text = exerciseFieldModel.nameExerscise)
-            Text(text = exerciseFieldModel.value.toString())
+            Text(
+                text = exerciseFieldModel.value.toString(),
+                modifier = Modifier.padding(15.dp)
+            )
         }
     }
 }
