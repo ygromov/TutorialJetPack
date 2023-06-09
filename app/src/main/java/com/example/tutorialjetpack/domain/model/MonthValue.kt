d7ae673e85f63a310de8b34aa2ee1914fbd3187a
test <gdcool@inbox.ru>
2023-06-09 12:28:53 +0300

    change init value

--- app/src/main/java/com/example/tutorialjetpack/domain/model/MonthValue.kt
+++ app/src/main/java/com/example/tutorialjetpack/domain/model/MonthValue.kt
@@ -1,9 +1,11 @@
 package com.example.tutorialjetpack.domain.model
 
 data class MonthValue(
-    val created: String,
-    val maxPull: Int,
-    val maxPush: Int,
-    val maxSquat: Int
+    var created: String = "Month",
+    var maxPull: Int = -1,
+    var maxPush: Int = -1,
+    var maxSquat: Int = -1,
+    var maxAbc: Int = -1,
+    var maxExtens: Int = -1
 )
 //error commit
