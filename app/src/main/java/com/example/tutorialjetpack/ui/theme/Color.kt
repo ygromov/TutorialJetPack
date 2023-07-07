57c7498e14f960c85190ed028fb160af3e847122
test <gdcool@inbox.ru>
2023-07-07 13:13:14 +0300

    added a new colors

--- app/src/main/java/com/example/tutorialjetpack/ui/theme/Color.kt
+++ app/src/main/java/com/example/tutorialjetpack/ui/theme/Color.kt
@@ -2,7 +2,10 @@ package com.example.tutorialjetpack.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Platinum200 = Color(0xFF857F7F)   //платиновый
+val Almond400 = Color(0xFFffe9ca) //очищенный миндаль
+val Grey300 = Color(0xFF242B32) //черно серый
+val Grey200 = Color(0xFF99C9FA)
+
+val Black900 = Color(0xFF000109)
+val White200 = Color(0xFFF7F8F8)
