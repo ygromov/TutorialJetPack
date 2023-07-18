52d75579cc4dcde8c504fa048a068c6efa27f97b
test <gdcool@inbox.ru>
2023-07-18 17:16:41 +0300

    changet a colors

--- app/src/main/java/com/example/tutorialjetpack/ui/theme/Color.kt
+++ app/src/main/java/com/example/tutorialjetpack/ui/theme/Color.kt
@@ -3,9 +3,9 @@ package com.example.tutorialjetpack.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Platinum200 = Color(0xFF857F7F)   //платиновый
-val Almond400 = Color(0xFFffe9ca) //очищенный миндаль
+val Yellow400 = Color(0xFFF3EC6F) //очищенный миндаль
 val Grey300 = Color(0xFF242B32) //черно серый
-val Grey200 = Color(0xFF99C9FA)
+val Red200 = Color(0xFFF13434)
 
 val Black900 = Color(0xFF000109)
-val White200 = Color(0xFFF7F8F8)
+val Orange200 = Color(0xFFEB845C)
