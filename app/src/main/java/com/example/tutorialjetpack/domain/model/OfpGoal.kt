c5283b864d5517b330e54d88788ef3bf1160ed2b
test <gdcool@inbox.ru>
2023-07-20 19:07:19 +0300

    created a new model for Analize OfpToGoal

---
+++ app/src/main/java/com/example/tutorialjetpack/domain/model/OfpGoal.kt
@@ -0,0 +1,9 @@
+package com.example.tutorialjetpack.domain.model
+
+data class OfpGoal(
+    var push: Int = -1,
+    var pull: Int = -1,
+    var squat: Int = -1,
+     var abs: Int = -1,
+     var extens: Int = -1
+)
