6e0a291a5299a11bf1aac261b021a0d47a05899e
test <gdcool@inbox.ru>
2023-07-20 19:11:40 +0300

    changed a button colors and added a few Text -

--- app/src/main/java/com/example/tutorialjetpack/presentation/screens/training_screen/TrainingScreen.kt
+++ app/src/main/java/com/example/tutorialjetpack/presentation/screens/training_screen/TrainingScreen.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
@@ -14,18 +17,24 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.tutorialjetpack.R
 import com.example.tutorialjetpack.presentation.screens.ProgressBar
 import com.example.tutorialjetpack.presentation.screens.training_screen.TrainingEvent
 import com.example.tutorialjetpack.presentation.screens.training_screen.TrainingState
+import com.example.tutorialjetpack.utils.Routers
 
 private const val TAG = "TrainingScreen"
 
@@ -61,7 +70,7 @@ fun TrainingScreen(
                         pressedElevation = 16.dp
                     ),
                     colors = ButtonDefaults.buttonColors(
-                        backgroundColor = MaterialTheme.colors.secondary,
+                        backgroundColor = MaterialTheme.colors.primaryVariant,
                         contentColor = MaterialTheme.colors.primary
                     ),
                     onClick = { onEvent.invoke(TrainingEvent.OneSet) }) {
@@ -75,7 +84,7 @@ fun TrainingScreen(
                         pressedElevation = 16.dp
                     ),
                     colors = ButtonDefaults.buttonColors(
-                        backgroundColor = MaterialTheme.colors.secondary,
+                        backgroundColor = MaterialTheme.colors.primaryVariant,
                         contentColor = MaterialTheme.colors.primary
                     ),
                     onClick = { onEvent.invoke(TrainingEvent.TwoSet) }) {
@@ -89,7 +98,7 @@ fun TrainingScreen(
                         pressedElevation = 16.dp
                     ),
                     colors = ButtonDefaults.buttonColors(
-                        backgroundColor = MaterialTheme.colors.secondary,
+                        backgroundColor = MaterialTheme.colors.primaryVariant,
                         contentColor = MaterialTheme.colors.primary
                     ),
                     onClick = { onEvent.invoke(TrainingEvent.ThreeSet) }) {
@@ -103,7 +112,7 @@ fun TrainingScreen(
                         pressedElevation = 16.dp
                     ),
                     colors = ButtonDefaults.buttonColors(
-                        backgroundColor = MaterialTheme.colors.secondary,
+                        backgroundColor = MaterialTheme.colors.primaryVariant,
                         contentColor = MaterialTheme.colors.primary
                     ),
                     onClick = { onEvent.invoke(TrainingEvent.FourSet) }) {
@@ -116,7 +125,26 @@ fun TrainingScreen(
                 textAlign = TextAlign.Center,
                 modifier = Modifier.padding(top = 16.dp), text = state.headText
             )
+            Card(
+                modifier = Modifier
+                    .padding(start = 10.dp, top = 10.dp, end = 10.dp)
+                    .background(MaterialTheme.colors.secondary)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colors.secondary)
+                        .padding(start = 20.dp, end = 20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
 
+                    ) {
+                    Text(text = "GIF", fontSize = 12.sp)
+                    Text(text = "Exersize", fontSize = 12.sp)
+                    Text(text = "Your goals", fontSize = 12.sp)
+                    Text(text = "How much?", fontSize = 12.sp)
+
+                }
+            }
             Card(
                 modifier = Modifier
                     .padding(start = 10.dp, top = 10.dp, end = 10.dp)
@@ -136,6 +164,22 @@ fun TrainingScreen(
                     )
                     Text(text = "Push Up")
                     Text(text = state.push.toString())
+                    Card() {
+                        TextField(
+                            value = "test", onValueChange = { newValue -> "test2" },
+                            maxLines = 1,
+                            modifier = Modifier
+                                .width(40.dp)
+                                .height(24.dp)
+                                .align(Alignment.CenterVertically),
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            colors = TextFieldDefaults.textFieldColors(
+                                textColor = MaterialTheme.colors.primary,
+                                backgroundColor = Color.White
+                            )
+                        )
+                    }
+
                 }
             }
             Card(
@@ -156,6 +200,21 @@ fun TrainingScreen(
                     )
                     Text(text = "Pull Up")
                     Text(text = state.pull.toString())
+                    Card() {
+                        TextField(
+                            value = "test", onValueChange = { newValue -> "test2" },
+                            maxLines = 1,
+                            modifier = Modifier
+                                .width(40.dp)
+                                .height(24.dp)
+                                .align(Alignment.CenterVertically),
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            colors = TextFieldDefaults.textFieldColors(
+                                textColor = MaterialTheme.colors.primary,
+                                backgroundColor = Color.White
+                            )
+                        )
+                    }
                 }
             }
             Card(
@@ -176,6 +235,21 @@ fun TrainingScreen(
                     )
                     Text(text = "Squat")
                     Text(text = state.squat.toString())
+                    Card() {
+                        TextField(
+                            value = "test", onValueChange = { newValue -> "test2" },
+                            maxLines = 1,
+                            modifier = Modifier
+                                .width(40.dp)
+                                .height(24.dp)
+                                .align(Alignment.CenterVertically),
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            colors = TextFieldDefaults.textFieldColors(
+                                textColor = MaterialTheme.colors.primary,
+                                backgroundColor = Color.White
+                            )
+                        )
+                    }
                 }
             }
             Card(
@@ -196,6 +270,21 @@ fun TrainingScreen(
                     )
                     Text(text = "Abs")
                     Text(text = state.abc.toString())
+                    Card() {
+                        TextField(
+                            value = "test", onValueChange = { newValue -> "test2" },
+                            maxLines = 1,
+                            modifier = Modifier
+                                .width(40.dp)
+                                .height(24.dp)
+                                .align(Alignment.CenterVertically),
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            colors = TextFieldDefaults.textFieldColors(
+                                textColor = MaterialTheme.colors.primary,
+                                backgroundColor = Color.White
+                            )
+                        )
+                    }
                 }
             }
             Card(modifier = Modifier.padding(start = 10.dp, top = 8.dp, end = 10.dp)) {
@@ -212,6 +301,21 @@ fun TrainingScreen(
                     )
                     Text(text = "Extens")
                     Text(text = state.extens.toString())
+                    Card() {
+                        TextField(
+                            value = "test", onValueChange = { newValue -> "test2" },
+                            maxLines = 1,
+                            modifier = Modifier
+                                .width(40.dp)
+                                .height(24.dp)
+                                .align(Alignment.CenterVertically),
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            colors = TextFieldDefaults.textFieldColors(
+                                textColor = MaterialTheme.colors.primary,
+                                backgroundColor = Color.White
+                            )
+                        )
+                    }
                 }
             }
 
@@ -223,11 +327,11 @@ fun TrainingScreen(
                     pressedElevation = 16.dp
                 ),
                 colors = ButtonDefaults.buttonColors(
-                    backgroundColor = MaterialTheme.colors.secondary,
+                    backgroundColor = MaterialTheme.colors.primaryVariant,
                     contentColor = MaterialTheme.colors.primary
                 ),
-                onClick = { navController.popBackStack() }) {
-                Text(text = "back")
+                onClick = { navController.navigate(Routers.DETAILS.route) }) {
+                Text(text = "to Home")
             }
         }
     }
