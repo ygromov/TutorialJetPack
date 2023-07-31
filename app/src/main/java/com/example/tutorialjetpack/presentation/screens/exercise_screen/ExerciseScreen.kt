67a5c2a5c41cce12c83f92b3cdedf0502fd3d8c5
test <gdcool@inbox.ru>
2023-07-31 10:18:21 +0300

    added a test code fo testing new registrationScreen

--- app/src/main/java/com/example/tutorialjetpack/presentation/screens/exercise_screen/ExerciseScreen.kt
+++ app/src/main/java/com/example/tutorialjetpack/presentation/screens/exercise_screen/ExerciseScreen.kt
@@ -1,38 +1,302 @@
 package com.example.tutorialjetpack.presentation.screens.exercise_screen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.tutorialjetpack.presentation.screens.exercise_screen.components.ExerciseItem
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun ExerciseScreen(
-    state: ExerciseState,
-    navController: NavController
+    exerciseState: ExerciseState,
+    navController: NavController,
+    onEvent: (ExersizeEvent) -> Unit,
 ) {
-    Column() {
-        ExerciseItem(exercise = state.list)
+    var state by remember { mutableStateOf(0) }
+    var appName by remember { mutableStateOf(TextFieldValue()) }
+    var userName by remember { mutableStateOf(TextFieldValue()) }
+    var height by remember { mutableStateOf(TextFieldValue()) }
+    var weight by remember { mutableStateOf(TextFieldValue()) }
+    var body by remember { mutableStateOf(TextFieldValue()) }
+    var activ by remember { mutableStateOf(TextFieldValue()) }
 
-        Button(
-            modifier = Modifier.padding(5.dp),
-            elevation = ButtonDefaults.elevation(
-                defaultElevation = 8.dp,
-                pressedElevation = 16.dp
-            ),
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = Color.White,
-                contentColor = Color.Black
-            ),
-            onClick = { navController.popBackStack() }) {
-            Text(text = "back")
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color.Black, Color.LightGray, Color.Green
+                        )
+                    ), alpha = 0.3f
+                ),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            when (state) {
+                0 -> EnterAppNameScreen(appName, onNameChange = { appName=it }) {
+                    state = 1
+                }
+
+                1 -> EnterUserNameScreen(userName, onWeightChange = { userName = it }) {
+                    state = 2
+                }
+
+                2 -> EnterHeightScreen(height, onWeightChange = { height = it }) {
+                    state =3// You can process the collected data here or reset the state for a new cycle
+                }
+
+                3 -> EnterWeightScreen(weight, onWeightChange = { weight = it }) {
+                    state = 4// You can process the collected data here or reset the state for a new cycle
+                }
+
+                4 -> EnterBodyScreen(body, onWeightChange = { body = it }) {
+                    state = 5
+                }
+
+                5 -> EnterActivScreen(activ, onWeightChange = { activ = it }) {
+                    state = 6
+                }
+            }
         }
     }
 }
-//error commit
+
+@Composable
+fun EnterAppNameScreen(
+    name: TextFieldValue,
+    onNameChange: (TextFieldValue) -> Unit,
+    onNextClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Придумай мне имя!", fontSize = 24.sp, color = Color.Black)
+        Spacer(modifier = Modifier.height(16.dp))
+        BasicTextField(
+            value = name,
+            onValueChange = onNameChange,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .height(56.dp).background(Color.White)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick =  onNextClick ,
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black)
+        ) {
+            Text("Присвоить имя", color = Color.White)
+        }
+    }
+}
+
+
+@Composable
+fun EnterUserNameScreen(
+    weight: TextFieldValue,
+    onWeightChange: (TextFieldValue) -> Unit,
+    onNextClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Мне нравится, а как тебя зовут?", fontSize = 24.sp, color = Color.Black)
+        Spacer(modifier = Modifier.height(16.dp))
+        BasicTextField(
+            value = weight,
+            onValueChange = onWeightChange,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .height(56.dp),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = onNextClick,
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black)
+        ) {
+            Text("Запомнить имя", color = Color.White)
+        }
+    }
+}
+
+@Composable
+fun EnterWeightScreen(
+    weight: TextFieldValue,
+    onWeightChange: (TextFieldValue) -> Unit,
+    onNextClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Красивое имя, а сколько ты весишь?", fontSize = 24.sp, color = Color.Black)
+        Spacer(modifier = Modifier.height(16.dp))
+        BasicTextField(
+            value = weight,
+            onValueChange = onWeightChange,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .height(56.dp),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = onNextClick,
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black)
+        ) {
+            Text("Запомнить вес", color = Color.White)
+        }
+    }
+}
+
+@Composable
+fun EnterHeightScreen(
+    weight: TextFieldValue,
+    onWeightChange: (TextFieldValue) -> Unit,
+    onNextClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            "Твоя форма лучше чем у 78% пользователей, а какой у тебя рост?",
+            fontSize = 24.sp,
+            color = Color.Black
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        BasicTextField(
+            value = weight,
+            onValueChange = onWeightChange,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .height(56.dp),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = onNextClick,
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black)
+        ) {
+            Text("Запомнить рост", color = Color.White)
+        }
+    }
+}
+
+@Composable
+fun EnterBodyScreen(
+    weight: TextFieldValue,
+    onWeightChange: (TextFieldValue) -> Unit,
+    onNextClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Отлично, а как ты оценишь свое телосложение?", fontSize = 24.sp, color = Color.Black)
+        Spacer(modifier = Modifier.height(16.dp))
+        BasicTextField(
+            value = weight,
+            onValueChange = onWeightChange,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .height(56.dp),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = onNextClick,
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black)
+        ) {
+            Text("Запомнить", color = Color.White)
+        }
+    }
+}
+
+@Composable
+fun EnterActivScreen(
+    weight: TextFieldValue,
+    onWeightChange: (TextFieldValue) -> Unit,
+    onNextClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            "Великолепно, а теперь оцени свою дневную активность, от 1 до 10!",
+            fontSize = 24.sp,
+            color = Color.Black
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        BasicTextField(
+            value = weight,
+            onValueChange = onWeightChange,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .height(56.dp),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = onNextClick,
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black)
+        ) {
+            Text("Класс, а теперь приступим!", color = Color.White)
+        }
+    }
+}
--- app/src/main/java/com/example/tutorialjetpack/presentation/screens/exercise_screen/ExerciseViewModel.kt
+++ app/src/main/java/com/example/tutorialjetpack/presentation/screens/exercise_screen/ExerciseViewModel.kt
@@ -4,10 +4,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.tutorialjetpack.domain.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,14 +15,74 @@ class ExerciseViewModel @Inject constructor(
     var state by mutableStateOf(ExerciseState())
 
     init {
-        getUserOfpData()
+        //getUserOfpData()
     }
 
+    fun onEvent(event: ExersizeEvent) {
+    when(event){
+        is ExersizeEvent.appName ->{
+            setAppName(event.value)
+        }
+        is ExersizeEvent.userName ->{
+            setUserName(event.value)
+        }
+        is ExersizeEvent.gender ->{
+            setGender(event.value)
+        }
+        is ExersizeEvent.age ->{
+            setAge(event.value)
+        }
+        is ExersizeEvent.height ->{
+            setHeight(event.value)
+        }
+        is ExersizeEvent.weight ->{
+            setWeight(event.value)
+        }
+        is ExersizeEvent.body ->{
+            setBody(event.value)
+        }
+        is ExersizeEvent.activ ->{
+            setActiv(event.value)
+        }
+    }
+    }
 
-    fun getUserOfpData() {
-        viewModelScope.launch {
-            repository.getUserOfp().collect {
-                    //resource ->
+    private fun setActiv(value: String) {
+        state = state.copy(activ = value)
+    }
+
+    private fun setBody(value: String) {
+        state = state.copy(body =  value)
+    }
+
+    private fun setWeight(value: String) {
+        state = state.copy(weight =  value)
+    }
+
+    private fun setHeight(value: String) {
+        state = state.copy(height =  value)
+    }
+
+    private fun setAge(value: String) {
+        state = state.copy(age = value)
+    }
+
+    private fun setGender(value: String) {
+        state = state.copy(gender =  value)
+    }
+
+    private fun setUserName(value: String) {
+        state = state.copy(userName =  value)
+    }
+
+    private fun setAppName(value: String) {
+        state = state.copy(appName = value)
+    }
+}
+//    fun getUserOfpData() {
+//        viewModelScope.launch {
+//            repository.getUserOfp().collect {
+//resource ->
 //                when (resource) {
 //                    is Resource.Success -> {
 //                        val ofpModel = resource.data                                        //записали данные из бд в переменную
@@ -44,11 +102,11 @@ class ExerciseViewModel @Inject constructor(
 //                        // Обработка ошибки, если необходимо
 //                    }
 //                    is Resource.Loading -> {
-                        // Обработка состояния загрузки, если необходимо
+// Обработка состояния загрузки, если необходимо
 //                    }
 //                }
-            }
-        }
-    }
-}
+//            }
+//        }
+//    }
+//}
 //error commit
