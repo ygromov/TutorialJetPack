86cb41aa5deb4877bfb5566e0468f12a3d1305ea
test <gdcool@inbox.ru>
2023-08-16 09:46:01 +0300

    changed colors again

--- app/src/main/java/com/example/tutorialjetpack/ui/theme/Color.kt
+++ app/src/main/java/com/example/tutorialjetpack/ui/theme/Color.kt
@@ -3,10 +3,14 @@ package com.example.tutorialjetpack.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Platinum200 = Color(0xFFEE0606)   //платиновый
-val Yellow400 = Color(0xFFECE9A3) //очищенный миндаль
-val Grey300 = Color(0xFF242B32) //черно серый
-val Red200 = Color(0xFFF44336)
 
-val Blue900 = Color(0xFF040C49) //button
-val Grey200 = Color(0xFFAFAAA9) //background
-val Black900 = Color(0xFF0C0200) //text
+val Grey300 = Color(0xFF242B32) //черно серый
+val Orange200 = Color(0xFFF44336)
+
+val Grey100 = Color(0xFFD3D3D3) //field
+val Blue900 = Color(0xFF061C6B) //button
+val Grey200 = Color(0xFFF2F2F2) //background
+val White900 = Color(0xFFFFFFFF) //text dark
+val Black600 = Color(0xFF222121) //text light
+
+val Green300 = Color(0xFF4CAF50) //progress bar
